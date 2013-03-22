b2407b77e3cb8f7d5c2710eacb937d73ee1afc7f
Suresh Khatri <servlet2@msn.com>
2013-03-23 00:14:33 +1100

    simple tests

---
+++ week1/1.py
@@ -0,0 +1,22 @@
+print """Try some Python"""
+
+#this is a comment. comment is to help us understand code's intention
+#use comment only if it helps explain something complicated
+
+print "Hello World! - Double Quotes"
+print 'Hello World - Single Quotes'
+print """Hello World - Triple Quotes"""
+
+#\n prints a new line
+#\t prints a tab
+print "Hi. " * 5
+print "Name: \tSuresh"
+print "City: \tHurstville"
+
+#addition
+print 2 + 4
+print "hi " + "there"
+
+#multiplication
+print "My Name is Suresh. \n" * 3
+
---
+++ week1/2.py
@@ -0,0 +1,33 @@
+"""
+Print your name
+Print your birthday
+The city you live in
+"""
+
+print "My name is Suresh"
+print "My birthday 1 Sept"
+print "City is Sydney"
+
+
+print "10 + 20"
+print 10 + 20
+
+name = "Suresh"
+Name = 'John'
+print name
+print Name
+
+#integer division
+print 5/2
+#operator precedence
+print 9 + 3 * 4
+
+#cube as multiplication
+print 2 * 2 * 2
+#exponential
+print 2 ** 3
+
+#storing results
+name="John"
+print name
+
