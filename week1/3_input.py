1a3b3bb5ddb1e9cbb91d2b36c4802d8c31a511ee
Suresh Khatri <servlet2@msn.com>
2013-03-23 00:35:59 +1100

    raw input to number

--- week1/3_input.py
+++ week1/3_input.py
@@ -5,3 +5,14 @@ name = raw_input("What is your name?")
 print name
 
 print "Hi", name
+
+
+#input integers
+#ask for cost price. sell price is 1 more than cost price
+cost_input = raw_input("what is the cost price? ")
+cost_price = float(cost_input)
+
+sell_price = cost_price + 1
+
+print "Cost Price", cost_price
+print "Sell Price", sell_price
