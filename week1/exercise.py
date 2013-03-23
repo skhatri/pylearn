e76c57af200310983792fa07ec2c14e3e4ec1813
Suresh Khatri <servlet2@msn.com>
2013-03-23 14:00:39 +1100

    homework added

---
+++ week1/exercise.py
@@ -0,0 +1,22 @@
+#Exercise - 1
+#Create a program that can help a user add two numbers
+# The program should ask user for the value of first number and then the second number.
+# It should calculate the sum of the two numbers and print the result.
+"""
+Example:
+
+    Enter first number: 2
+    Enter second number: 3
+    The sum of 2 and 3 is 5
+"""
+
+#Exercise - 2
+#Create a program which asks the user for temperature in Celsius and prints the farenheit result
+# Formula: F = 9/5 * C + 32
+"""
+Example:
+
+Enter temperature in Celcius: 37.5
+Farenheit equivalent of 37.5 Celcius is 99.5
+Thank you for using my temperature calculator
+"""
--- week1/homework.py
+++ week1/homework.py
@@ -1,7 +1,7 @@
 #Homework - 1
-#Create a program that can help a user add two numbers
+#Create a program that can help a user perform exponential calculations.
 # The program should ask user for the value of first number and then the second number.
-# It should calculate the sum of the two numbers and print the result.
+# It should calculate the result of first number to the power of second number.
 """
 Example:
 
@@ -10,17 +10,8 @@ Example:
     The sum of 2 and 3 is 5
 """
 
+
 #Homework - 2
-#Create a program which asks the user for temperature in Celsius and prints the farenheit result
-"""
-Example:
-
-Enter temperature in Celcius: 37.5
-Farenheit equivalent of 37.5 Celcius is 99.5
-Thank you for using my temperature calculator
-"""
-
-#Homework - 3
 #Create another program which asks the user for temperature in Farenheit and prints the celcius result
 """
 Example:
@@ -33,7 +24,7 @@ Thank you for using my temperature calculator
 #Note: the input temperatures have to be stored in variables and the calculator should
 #work for any given input, not just 37.5 and 99.5.
 
-#Homework - 4
+#Homework - 3
 #Create a program which prompts the user for a diameter of a circle. It then calculates and prints the area of the circle.
 """
 Example
